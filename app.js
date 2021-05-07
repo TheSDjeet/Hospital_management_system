23f7c617c82455057a8bed01dfad633efdbc0fcd
Shubhajeet <sdjeet97@gmail.com>
2021-05-07 20:28:07 +0530

    Layout tested.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+//const bootstrap = require('bootstrap');
 
 const app = express();
 
@@ -13,9 +14,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get("/", function (req, res) {
-    //res.render("home");
-    res.sendFile(__dirname + "index.html");
+    res.render("home");
+    //res.sendFile(__dirname + "index.html");
 });
+
+app.get("/support", function (req, res) {
+    res.render("support");
+});
+
+app.get("/help", function (req, res) {
+    res.render("help");
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
